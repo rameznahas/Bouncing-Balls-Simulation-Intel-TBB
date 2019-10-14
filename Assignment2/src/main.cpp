9ae98112fa54064ef480d17c9f8d6578f48c900b
rameznahas <rameznahas@gmail.com>
2019-10-14 12:09:27 -0400

    quality of life change

--- Assignment2/src/main.cpp
+++ Assignment2/src/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include "bouncing_balls_sim.h"
 
-bouncing_balls_sim sim;
+bouncing_balls_sim* p_sim;
 
 int main(int argc, char **argv) {
-	sim = bouncing_balls_sim(&argc, argv);
-	sim.start([]() {
-		sim.update(); 
+	bouncing_balls_sim sim(&argc, argv);
+	p_sim = &sim;
+	p_sim->start([]() {
+		p_sim->update();
 	});
 
 	return 0;
